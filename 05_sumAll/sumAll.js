450d0ca16755a2db7f17b52cb07eb9c914678655
henryPC <henbrooks6@gmail.com>
2022-04-23 14:30:31 +0100

    finished exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum = 0;
+    //orders correctly
+    if (start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
 
+    //if negative or not interger return error
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    }
+
+    for (let i = start; i <= end; i ++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (temp - 32) * (5/9);
+  newTemp = Math.round(newTemp * 10) / 10
+  return newTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = temp * (9/5) + 32;
+  newTemp = Math.round(newTemp * 10) / 10;
+  return newTemp;
 };
 
 // Do not edit below this line
